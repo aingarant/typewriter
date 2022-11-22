514591f1dee39205dd9c32417c005543fadf33b6
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-22 10:25:43 -0500

    Added new line to typewriter.

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,15 @@
 const typewriter = (sentence) => {
+  let delay = 1000;
   const chars = sentence.split("");
   chars.map((char, index) => {
     setTimeout(() => {
       process.stdout.write(char);
-    }, index * 50);
-  })
+    }, delay);
+    delay = delay+50;
+  });
+  setTimeout(()=>
+  {
+    process.stdout.write("\n");
+  },delay);  
 }
+typewriter("This is the sentence.");
